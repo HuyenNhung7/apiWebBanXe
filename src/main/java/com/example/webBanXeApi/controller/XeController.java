7588bb92663002b21bf548b548077da04acc6751
Phuc Hoang <hoangmaithienphuc@gmail.com>
2023-04-19 23:55:01 +0700

    Refactor Xe api added Xe API documentation, rename User class in HoaDonController

--- src/main/java/com/example/webBanXeApi/controller/XeController.java
+++ src/main/java/com/example/webBanXeApi/controller/XeController.java
@@ -17,63 +17,48 @@ public class XeController {
     @Autowired
     private IXeService iXeService;
 
+    @GetMapping
+    public ResponseEntity<XeDto> findXeByTenAndThuongHieu(@RequestParam(required = false) String ten,
+                                                          @RequestParam(required = false) String thuongHieu,
+                                                          @RequestParam(defaultValue = "0") int page,
+                                                          @RequestParam(defaultValue = "100") int size)
+    {
+        List<Xe> results;
+        if (ten == null && thuongHieu == null) {
+            results = iXeService.getAllXeByPage(page, size);
+        } else {
+            results = iXeService.getXeByNameAndBranch(ten, thuongHieu, page, size);
+        }
+        return ResponseEntity.status(HttpStatus.OK).body(
+                new XeDto(results, iXeService.getAllXe().size(), page, size)
+        );
+    }
+
     // API add xe
-    // http://localhost:8080/api/v1/xe/add
-    @PostMapping("/add")
+    // http://localhost:8080/api/v1/xe/
+    @PostMapping
     public Xe addXe(@RequestBody Xe xe){
         return iXeService.addXe(xe);
     }
 
     // API update xe
-    // http://localhost:8080/api/v1/xe/update?id=2
-    @PutMapping("/update")
-    public Xe updateXe(@RequestParam long id, @RequestBody Xe xe){
+    // http://localhost:8080/api/v1/xe/1
+    @PutMapping("/{id}")
+    public Xe updateXe(@PathVariable Long id, @RequestBody Xe xe){
         return iXeService.updateXe(id, xe);
     }
 
-    // API lấy ra tất cả xe
-    // http://localhost:8080/api/v1/xe/all
-    @GetMapping("/all")
-    public ResponseEntity<XeDto> getAllXe() {
-        List<Xe> result= iXeService.getAllXe();
-        return ResponseEntity.status(HttpStatus.OK).body(
-                new XeDto(result, iXeService.getAllXe().size(), 0, result.size())
-        );
-    }
-
     //API lấy ra 1 xe theo id
-    // http://localhost:8080/api/v1/xe?id=1
-    @GetMapping("")
-    public Xe getOneXeById(@RequestParam long id) {
+    // http://localhost:8080/api/v1/xe/1
+    @GetMapping("/{id}")
+    public Xe getOneXeById(@PathVariable Long id) {
         return iXeService.getOneXeById(id);
     }
 
     // API xóa một xe theo id
-    // http://localhost:8080/api/v1/xe/delete?id=1
-    @DeleteMapping("/delete")
+    // http://localhost:8080/api/v1/xe/1
+    @DeleteMapping("/{id}")
     public boolean deleteXe(@RequestParam long id){
-           return iXeService.deleteXe(id);
-    }
-
-    // API lấy xe theo trang
-    // http://localhost:8080/api/v1/xe/page?page=0&size=2
-    @GetMapping("/page")
-    public ResponseEntity<XeDto> getAllXeByPage(@RequestParam int page, @RequestParam int size){
-        List<Xe> results = iXeService.getAllXeByPage(page, size);
-        return ResponseEntity.status(HttpStatus.OK).body(
-                new XeDto(results, iXeService.getAllXe().size(), page, size)
-        );
-    }
-
-    @GetMapping("/find")
-    public ResponseEntity<XeDto> findXeByTenAndThuongHieu(@RequestParam(required = false) String ten,
-                                                 @RequestParam(required = false) String thuongHieu,
-                                                 @RequestParam(defaultValue = "0") int page,
-                                                 @RequestParam(defaultValue = "0") int size)
-    {
-        List<Xe> results = iXeService.getXeByNameAndBranch(ten, thuongHieu, page, size);
-        return ResponseEntity.status(HttpStatus.OK).body(
-                new XeDto(results, iXeService.getAllXe().size(), page, size)
-        );
+        return iXeService.deleteXe(id);
     }
 }
