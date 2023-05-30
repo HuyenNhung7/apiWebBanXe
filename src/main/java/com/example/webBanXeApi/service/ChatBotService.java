b9ee38c8b0274e95a65c769b423f9d3861256c1a
Phuc Hoang <hoangmaithienphuc@gmail.com>
2023-05-30 10:59:35 +0700

    update ChatBotService

--- src/main/java/com/example/webBanXeApi/service/ChatBotService.java
+++ src/main/java/com/example/webBanXeApi/service/ChatBotService.java
@@ -2,59 +2,70 @@ package com.example.webBanXeApi.service;
 
 import com.theokanning.openai.completion.CompletionRequest;
 import com.theokanning.openai.service.OpenAiService;
+import com.theokanning.openai.completion.chat.*;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
+import java.util.concurrent.atomic.AtomicBoolean;
 
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
 public class ChatBotService {
     public static void main(String[] args) {
         // Press Alt+Enter with your caret at the highlighted text to see how
         // IntelliJ IDEA suggests fixing it.
-        System.out.println("Hello and welcome!");
-        List<String> myList = new ArrayList<>();
-        myList.add(".");
-        myList.add("?");
-        myList.add("!");
-        myList.add("\n");
+        System.out.println("Demo chatbot!");
 
-
-        String prompt = "The following is a conversation with an AI assistant. The assistant is helpful, creative, clever, and very friendly." +
-                "The assistant should only answer question in one sentence." +
-                "Human: Hello, who are you? " +
-                "AI: I am an AI created by OpenAI. How can I help you today? ";
+        String promptParameters = "Give me a brief and clear answer in 50 words or less: ";
 
         OpenAiService service = new OpenAiService("");
 
         // Create a loop to get user input and generate chatbot responses
         Scanner scanner = new Scanner(System.in);
-        while (true) {
-            // Get user input
-            System.out.print("Human: ");
-            String userInput = scanner.nextLine();
+            final List<ChatMessage> messages = new ArrayList<>();
+            final ChatMessage systemMessage = new ChatMessage(ChatMessageRole.SYSTEM.value(), promptParameters + "should i buy a new car or a used one?");
+            messages.add(systemMessage);
 
-            // Append user input to the prompt
-            prompt += "Human: " + userInput + " ";
-
-            // Create a completion request with the prompt and some parameters
-            CompletionRequest completionRequest = CompletionRequest.builder()
-                    .prompt(prompt)
-                    .maxTokens(50)
-                    .model("ada")
-                    .stop(myList)
+            ChatCompletionRequest chatCompletionRequest = ChatCompletionRequest
+                    .builder()
+                    .model("gpt-3.5-turbo")
+                    .messages(messages)
+                    .n(1)
+                    .stop(Collections.singletonList("\n"))
+                    .maxTokens(100)
+                    .logitBias(new HashMap<>())
                     .build();
-            // Call the service to create a completion and
-            // Extract the generated text from the response
-            String generatedText = service.createCompletion(completionRequest).getChoices().get(0).getText();
 
-            System.out.println(prompt);
+            StringBuilder concatenatedContent = new StringBuilder();
 
-            // Append the generated text to the prompt
-            prompt += "AI: " + generatedText + "\n";
+            AtomicBoolean stopCondition = new AtomicBoolean(false);
 
-            // Print the generated text as the chatbot's reply
-            System.out.println("AI: " + generatedText);
+            service.streamChatCompletion(chatCompletionRequest)
+                    .doOnError(throwable -> {
+                        throwable.printStackTrace();
+                        stopCondition.set(true);
+                        service.shutdownExecutor();
+                    })
+                    .takeWhile(chunk -> {
+                        ChatCompletionChoice choice = chunk.getChoices().get(0);
+                        String finishReason = choice.getFinishReason();
+                        if (finishReason != null && finishReason.equals("length")) {
+                            stopCondition.set(true);
+                            service.shutdownExecutor();
+                            return false;
+                        }
+                        return !stopCondition.get();
+                    })
+                    .blockingForEach(chunk -> {
+                        ChatMessage message = chunk.getChoices().get(0).getMessage();
+                        if (message.getContent() != null) {
+                            // Process the content here
+                            concatenatedContent.append(message.getContent());
+                        }
+                    });
+
+
+
+            String finalContent = concatenatedContent.toString();
+            System.out.println(finalContent);
         }
-
-    }
 }
