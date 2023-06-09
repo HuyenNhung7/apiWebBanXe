a40fd9dc421d8e3a0d6c3c8dac6bfa5da742937f
Phuc Hoang <hoangmaithienphuc@gmail.com>
2023-06-09 10:21:10 +0700

    complete askbot

--- src/main/java/com/example/webBanXeApi/controller/AskBotController.java
+++ src/main/java/com/example/webBanXeApi/controller/AskBotController.java
@@ -1,31 +1,28 @@
-package com.example.webBanXeApi.service;
+package com.example.webBanXeApi.controller;
 
 import com.theokanning.openai.service.OpenAiService;
 import com.theokanning.openai.completion.chat.*;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.*;
 
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
-public class ChatBotService {
-    public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
+@RestController
+public class AskBotController {
+
+    @PostMapping("/api/v1/ask")
+    public Map<String, String> processQuestion(@RequestBody String question, @RequestParam(defaultValue = "") String context) {        // Press Alt+Enter with your caret at the highlighted text to see how
         // IntelliJ IDEA suggests fixing it.
-        System.out.println("Hello and welcome!");
-        List<String> myList = new ArrayList<>();
-        myList.add(".");
-        myList.add("?");
-        myList.add("!");
-        myList.add("\n");
 
         String promptParameters = "Give me a brief and clear answer in 50 words or less: ";
 
 
-        OpenAiService service = new OpenAiService("YOUR-OPENAI-API");
+        OpenAiService service = new OpenAiService("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");
 
 
         final List<ChatMessage> messages = new ArrayList<>();
-        final ChatMessage systemMessage = new ChatMessage(ChatMessageRole.SYSTEM.value(), promptParameters + "should i buy a new car or a used one?");
+        final ChatMessage systemMessage = new ChatMessage(ChatMessageRole.SYSTEM.value(), promptParameters + context + " " + question);
         messages.add(systemMessage);
 
         ChatCompletionRequest chatCompletionRequest = ChatCompletionRequest
@@ -54,6 +51,10 @@ public class ChatBotService {
 
         System.out.println(finalContent);
         service.shutdownExecutor();
+
+        HashMap<String, String> response = new HashMap<>();
+        response.put("answer", finalContent);
+        return response;
     }
 
 }
