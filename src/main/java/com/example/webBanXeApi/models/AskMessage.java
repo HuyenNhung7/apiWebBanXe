c67ff4b100431e17d0fef9a3b45a59a5c9f49444
Phuc Hoang <71083508+mcbutt007@users.noreply.github.com>
2023-06-11 00:44:02 +0700

    fix bugs in chatbot (#13)
    * askbot works

---
+++ src/main/java/com/example/webBanXeApi/models/AskMessage.java
@@ -0,0 +1,14 @@
+package com.example.webBanXeApi.models;
+
+public class AskMessage {
+    String question;
+    String context;
+
+    public String getContext() {
+        return context;
+    }
+
+    public String getQuestion() {
+        return question;
+    }
+}
